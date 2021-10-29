361aa809281198ba5b416627a0165fb683a78942
thePianoKid <gbraden.dev@gmail.com>
2021-10-29 01:00:07 -0400

    added basic documentation

--- main.py
+++ main.py
@@ -4,23 +4,32 @@ from pynput.keyboard import Key, Listener
 r = sr.Recognizer()
 
 def listen_for_cmd():
+    """
+    Convert's the user's speech to text
+
+    Using the SpeechRecognition library (https://github.com/Uberi/speech_recognition), 
+    the microphone records the user's voice and converts it to usable text. 
+    
+    The program can also run in the background.
+    """
     with sr.Microphone() as source:
-        print("Say something!")
+        print("\nSay a command")
         audio = r.listen(source)
 
-    # recognize speech using Google Speech Recognition
     try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+        # TODO: add functionality to the user's commands
+        print("Google speech recognition thinks you said " + r.recognize_google(audio))
     except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
+        print("Google speech recognition could not understand audio")
     except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        print(f"Could not request results from Google speech recognition service; {e}")
 
 
 def activate_assistant(key):
+    """
+    This function waits for the f4 key to be pressed, then it fires the listen_for_cmd function
+    """
+
     if key == Key.f4:
         listen_for_cmd()
           
@@ -29,5 +38,7 @@ def activate_assistant(key):
     if key == Key.delete: 
         return False
 
+
 with Listener(on_press = activate_assistant) as listener:
+    print('When you are ready to speak a command, press the F4 key: ')
     listener.join()
